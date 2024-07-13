3e782baa14119d1a882c331dd11e6a071e21c56a
Matias Figueroa <matiasarielfigueroa@gmail.com>
2024-07-13 19:27:32 -0300

    rename

--- src/main/java/com/meli/middleend/MeliMiddleendApplication.java
+++ src/main/java/com/meli/middleend/MeliMiddleendApplication.java
@@ -1,13 +1,13 @@
-package com.meli.demo;
+package com.meli.middleend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestMiddleendApplication {
+public class MeliMiddleendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestMiddleendApplication.class, args);
+		SpringApplication.run(MeliMiddleendApplication.class, args);
 	}
 
 }
