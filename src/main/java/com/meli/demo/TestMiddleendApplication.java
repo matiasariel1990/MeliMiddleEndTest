be191923d4cefe713626a5c8f597c106cbf1789b
Matias Figueroa <matiasarielfigueroa@gmail.com>
2024-07-13 19:13:18 -0300

    Commit inicial

---
+++ src/main/java/com/meli/demo/TestMiddleendApplication.java
@@ -0,0 +1,13 @@
+package com.meli.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMiddleendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMiddleendApplication.class, args);
+	}
+
+}
