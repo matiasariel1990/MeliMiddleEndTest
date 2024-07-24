64677f99fa799065f8665e66c41dfae420289403
Matias Figueroa <matiasarielfigueroa@gmail.com>
2024-07-24 20:05:13 -0300

    Set properties and add validator to controller

--- src/main/java/com/meli/middleend/controller/impl/ItemControllerImpl.java
+++ src/main/java/com/meli/middleend/controller/impl/ItemControllerImpl.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemControllerImpl implements ItemController {
 
+    private static final int DEFAULT_OFFSET = 0;
+
+
     @Autowired
     ItemService itemService;
 
@@ -23,12 +26,24 @@ public class ItemControllerImpl implements ItemController {
     @Override
     public ResponseEntity<PageItemResponse> getItemBySite(String site, String query, int offset, int limit, String sortBy) {
         QueryDto queryDto = QueryDto.builder().build();
+        queryDto.setSiteEnum(validatorService.validarSite(site));
+        queryDto.setQuery(validatorService.validarQuery(query));
+        if(offset != DEFAULT_OFFSET){
+            queryDto.setOffset(validatorService.validarOffset(offset));
+        }
+        queryDto.setLimit(validatorService.validarLimit(limit));
+        if(sortBy != null){
+            queryDto.setSortEnum(validatorService.validarSort(sortBy));
+        }
+
         return new ResponseEntity<>(itemService.getItemsByQuery(queryDto), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<ItemResponse> getItem(String id) {
 
-        return new ResponseEntity<>(itemService.getItemById(id), HttpStatus.OK);
+        return new ResponseEntity<>(itemService.getItemById(
+                validatorService.validarId(id)
+        ), HttpStatus.OK);
     }
 }
